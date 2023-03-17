4c340acf935f0637dba9a15a026cefd70900abf2
JeremyLeening <jeremy.leening@calco.nl>
2023-03-17 10:17:56 +0100

    Initials Challange

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,15 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+
+         System.out.println("JJJJJ   A    L    ");
+         System.out.println("  J    A A   L    ");
+         System.out.println("  J   A   A  L    ");
+         System.out.println("  J   AAAAA  L    ");
+         System.out.println("  J   A   A  L    ");
+         System.out.println("J J   A   A  L    ");
+         System.out.println(" JJ   A   A  LLLLL");
+
+
     }
 }
