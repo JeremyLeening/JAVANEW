209da711015ed726317293a800599533bc58edde
JeremyLeening <124164175+JeremyLeening@users.noreply.github.com>
2023-03-27 22:28:22 +0200

    home1

--- Photoshop/View/Main.java
+++ Photoshop/View/Main.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
-public static void Main(String[] args) {
+public static void main(String[] args) {
 
+    CSVFileReader reader = new CSVFileReader("_resources/PhotoShop_PriceList.csv", ",");
+    reader.read();
+    
     Scanner scan = new Scanner(System.in);
     System.out.println("Welcome to the Photoshop store!");
     System.out.println("What would you like to do");
